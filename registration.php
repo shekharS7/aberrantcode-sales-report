10f73d83f0296e6900ff42a238bfb7841c50c13c
Shekar Suman <shekharsuman97@gmail.com>
2024-08-03 21:57:10 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,9 +1,14 @@
 <?php
 /**
- * Copyright © Boostmyshop All rights reserved.
+ * Copyright © Shekhar Suman, 2024. All rights reserved.
  * See COPYING.txt for license details.
+ * 
+ * @package     AberrantCode_CustomerImport
+ * @version     1.0.0
+ * @license     MIT License (http://opensource.org/licenses/MIT)
+ * @autor       Shekhar Suman
  */
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'BmsIndia_Report', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Aberrant_Report', __DIR__);
 
